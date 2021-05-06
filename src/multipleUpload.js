2f7193337596099a2ef04717365402376189b326
Leonardo Sousa <leonardolsousa5@gmail.com>
2021-05-06 19:23:10 -0300

    Setando o app

---
+++ src/multipleUpload.js
@@ -0,0 +1,9 @@
+<form method="POST" action="/upload-multiple-images" enctype="multipart/form-data">
+    <div>
+        <label>Select multiple images:</label>
+        <input type="file" name="multiple_images" multiple />
+    </div>
+    <div>
+        <input type="submit" name="btn_upload_multiple_images" value="Upload" />
+    </div>
+</form>
